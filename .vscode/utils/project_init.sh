c42a4cb67de5dc2bbc4377a15ea29864ad9e2cd7
R3D9477 <r3d9477@google.com>
2025-06-01 15:46:39 +0200

    fix init script

--- .vscode/utils/project_init.sh
+++ .vscode/utils/project_init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 if ! [ -f Cargo.toml ] ; then
-    sed -i 's/\r$//' .vscode/utils/*.sh
-    sudo chown -R build:build .
-
     cargo init
     cargo new lib --lib
 
     echo "lib = { path = 'lib' }" >> "Cargo.toml"
+
     echo -e "use lib;\n\n$(cat src/main.rs)" > src/main.rs
     sed -i 's/"Hello, world!"/"Hello, world from lib {}!", lib::add(1, 2)/g' src/main.rs
 fi
